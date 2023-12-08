612965bba4f53f1b9326cae4bfd6a164bbdcbc75
Jamie Holloway <jamie@cadiahq.com>
2023-12-08 12:03:54 +0000

    solved primes

--- primes.py
+++ primes.py
@@ -1,6 +1,21 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+import math
 
 def primes(number_of_primes):
-    list = []
+    if number_of_primes < 1:
+        raise ValueError('Work with Positive Numbers Only')
+    list = [2]
+    count = 3
+    while (len(list) < number_of_primes):
+        isPrime = True
+        for i in range(2, math.floor(count / 2)):
+            if count % i == 0:
+                count += 2
+                isPrime = False
+                break
+        if isPrime:
+            list.append(count)
+            count += 2
     return list
+
